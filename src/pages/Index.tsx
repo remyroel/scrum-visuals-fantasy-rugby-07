2459d10f3f2e8729a0e3758f98794a35cef13460
Remy Roel <112566889+remyroel@users.noreply.github.com>
2025-04-21 15:49:28 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -81,7 +81,7 @@ const Index: React.FC = () => {
           >
             <span className="text-[#003366]">FANTASY RUGBY</span>
             <br />
-            <span className="text-[#FFC700] text-3xl md:text-5xl">IS ABOUT TO GET <br />SCRUMMY</span>
+            <span className="text-[#FFC700] text-3xl md:text-5xl">REIMAGINED</span>
           </motion.h1>
 
           <motion.p
